8c4ab4acb7ff751a24f23d7e51811d6e4305775a
Shweta Yadav <shwetay629@gmail.com>
2022-02-28 12:35:00 +0530

    admin patients list bug fixed

--- resources/views/Frontend/admin/admin_patients_list.blade.php
+++ resources/views/Frontend/admin/admin_patients_list.blade.php
@@ -25,38 +25,48 @@
                     <div class="card-body">
                         <div class="table-responsive">
                             <div class="table-responsive">
-                            <table class="datatable table table-hover table-striped table-center mb-0">
-                                <thead>
+                                <table class="datatable table table-hover table-striped table-center mb-0">
+                                    <thead>
 
-                                    <tr style="background-color:#09e5ab; color:white; ">
-                                        <th>Patient ID</th>
-                                        <th>Patient Name</th>
-                                        <th>Date of birth</th>
-                                        <th>Address</th>
-                                        <th>Phone</th>
+                                        <tr style="background-color:#09e5ab; color:white; ">
+                                            <th>Patient ID</th>
+                                            <th>Patient Name</th>
+                                            <th>Date of birth</th>
+                                            <th>Address</th>
+                                            <th>Phone</th>
 
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($patients as $patient )
-                                    <tr>
-                                        <td>#PT00{{ $patient-> id}}</td>
-                                        <td>
-                                            <h2 class="table-avatar">
-                                                <a href="profile.html" class="avatar avatar-sm mr-2"><img class="avatar-img rounded-circle" src="/uploads/Images/{{$patient->profile_photo}}" alt="User Image"></a>
-                                                <a href="profile.html">{{$patient->name}}</a>
-                                            </h2>
-                                        </td>
-                                        <td>{{$patient->dob}}</td>
-                                        <td>{{$patient->address}}</td>
-                                        <td>{{$patient->phone}}</td>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($patients as $patient)
+                                            <tr>
+                                                <td>#PT00{{ $patient->id }}</td>
+                                                <td>
+                                                    <h2 class="table-avatar">
+                                                        <a href="profile.html" class="avatar avatar-sm mr-2">
+                                                            @if ($patient->profile_photo === null)
+                                                                <img src="{{ asset('/uploads/Images/default.png') }}"
+                                                                    alt="User Image" class="avatar-img rounded-circle">
+                                                            @else
+                                                                <img class="avatar-img rounded-circle"
+                                                                    src="/uploads/Images/{{ $patient->profile_photo }}"
+                                                                    alt="User Image">
+                                                            @endif
 
-                                    </tr>
-                                    @endforeach
-                                </tbody>
+                                                        </a>
+                                                        <a href="profile.html">{{ $patient->name }}</a>
+                                                    </h2>
+                                                </td>
+                                                <td>{{ $patient->dob }}</td>
+                                                <td>{{ $patient->address }}</td>
+                                                <td>{{ $patient->phone }}</td>
 
-                            </table>
-                        </div>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
